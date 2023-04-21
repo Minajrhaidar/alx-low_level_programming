c70e48c26ccff942ad321d7eac06abfe7c5a78f7
Mina jrhaidar <amina.jrhaidar@gmail.com>
2023-04-21 05:35:49 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints the alphabet in lowercase except q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}
