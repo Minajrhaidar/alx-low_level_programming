6f53f62251ef2844976e2248640c766826c622a5
Mina jrhaidar <amina.jrhaidar@gmail.com>
2023-04-25 09:50:34 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code
- * Return: Always 0.
+ * _abs - computes the abs value of an int
+ * @n: the int in question
+ *
+ * Return: the abs of the int.
  */
-int main(void)
+int _abs(int n)
 {
-int r;
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+if (n >= 0)
+return (n);
+else
+return (-n);
 }
