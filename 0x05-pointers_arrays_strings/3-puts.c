17b76c5737f2b68d305d4e91b8fe68088ae75177
Mina jrhaidar <amina.jrhaidar@gmail.com>
2023-05-02 07:29:15 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,11 +6,9 @@
  */
 void _puts(char *str)
 {
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+for (; *str != '\0'; str++)
+{
+_putchar(*str);
+}
+_putchar('\n');
 }
