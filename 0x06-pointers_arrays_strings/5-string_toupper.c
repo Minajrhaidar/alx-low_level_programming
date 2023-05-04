2d2bb10087a629e2ee3076acda86b0044d6df069
Mina jrhaidar <amina.jrhaidar@gmail.com>
2023-05-04 06:52:00 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: pointer to string to modify
+ *
+ * Return: pointer to modified string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr -= 32;
+		}
+		ptr++;
+	}
+
+	return (str);
+}
