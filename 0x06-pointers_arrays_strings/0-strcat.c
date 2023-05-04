612bac4cc6805030af5306492b1e58968a90c38b
Mina jrhaidar <amina.jrhaidar@gmail.com>
2023-05-04 05:47:56 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
-
-	/* Find the length of the destination string */
-	while (dest[index++])
-		dest_len++;
-
-	/* Append the source string to the destination string */
-	for (index = 0; src[index]; index++)
-		dest[dest_len ++] = src[index];
-	return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len ++] = src[index];
+return (dest);
 }
